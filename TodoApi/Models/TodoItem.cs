50e7dbf3289c9259f7b1df87596f7bd5401e7c67
Daniel Winn <146897399+Danie1Winn@users.noreply.github.com>
2026-03-29 16:57:02 -0700

    config(Program.cs): enable static files and register TodoContext.

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -2,9 +2,13 @@
 {
     public class TodoItem
     {
+        // identifier for each todo item
         public long Id { get; set; }
+        // description / name of item
         public string? Name { get; set; }
+        // flags if item is completed or not
         public bool IsComplete { get; set; }
+        // tracks the date a task was finished, can't be null
         public DateTime? CompletionDate { get; set; }
     }
 }
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,23 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// adds services to container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// register the database context suing "TodoList"
+// stores data in RAM while app runs
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// HTTP request pipeline config
+if (builder.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
 
+// enable default file mapping
+app.UseDefaultFiles();
+// allows app to serve static files from wwwroot
+app.UseStaticFiles();
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
